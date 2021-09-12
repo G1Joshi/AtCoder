178befcb443935639bbe3e1702e4b3928baca37f
G1Joshi <joshijeevan98@gmail.com>
2021-09-12 06:46:01 +0530

    add A

---
+++ Contests/ABC217/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    if (s < t)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
